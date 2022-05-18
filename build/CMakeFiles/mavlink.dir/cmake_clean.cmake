91a9b39e29a94ae3e1c6b66a50f8d1da302ff518
ww <1113870631@qq.com>
2022-05-18 15:39:24 +0800

    first commit

---
+++ build/CMakeFiles/mavlink.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mavlink.dir/main.cpp.o"
+  "mavlink.pdb"
+  "mavlink"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mavlink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
